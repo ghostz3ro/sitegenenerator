d3b9f58e77ae1584ad94a27452b62daedb3ceb1a
ghostz3ro <thasotiropoulos@gmail.com>
2025-09-07 17:11:46 +0300

    added tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,3 +1,4 @@
+from platform import node
 import unittest
 
 from textnode import TextNode, TextType
@@ -7,8 +8,19 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+        node3 = TextNode("This is a different text node", TextType.BOLD)
+        node4 = TextNode("This is a text node", TextType.ITALIC)
 
+        node5 = "Not a TextNode"
+        node6 = TextNode("This is a text node", TextType.BOLD, url="https://example.com")
+        node7 = TextNode("This is a text node", TextType.BOLD, url="https://example.gr")
+
+        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertNotEqual(node, node4)
+        self.assertNotEqual(node, node5)
+        self.assertNotEqual(node6, node7)
+        self.assertNotEqual(node, node7)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -5,6 +5,15 @@ class TextType(Enum):
     LINK = "link"
     PARAGRAPH = "paragraph"
     HEADING = "heading"
+    BOLD = "bold"
+    ITALIC = "italic"
+    UNDERLINE = "underline"
+    STRIKETHROUGH = "strikethrough"
+    CODE = "code"
+    QUOTE = "quote"
+    LIST_ITEM = "list_item"
+    ORDERED_LIST_ITEM = "ordered_list_item"
+    UNORDERED_LIST_ITEM = "unordered_list_item"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -13,9 +22,9 @@ class TextNode:
         self.url = url
 
 
-    def __eq__(self, obj1, obj2):
-        if isinstance(obj1, TextNode) and isinstance(obj2, TextNode):
-            return obj1.text == obj2.text and obj1.text_type == obj2.text_type and obj1.url == obj2.url
+    def __eq__(self, obj2):
+        if isinstance(self, TextNode) and isinstance(obj2, TextNode):
+            return self.text == obj2.text and self.text_type == obj2.text_type and self.url == obj2.url
         return False
     
     def __repr__(self):
