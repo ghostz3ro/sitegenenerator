c24f3d68af681a86fba879ee57ebbbe796245686
ghostz3ro <thasotiropoulos@gmail.com>
2025-09-09 00:50:20 +0300

    file management

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-python3 src/test.sh
+import os
+import shutil
+
+def copy_to_directory(src, dest):
+    
+    if not os.path.exists(src):
+        raise Exception(f"Source directory '{src}' does not exist.")
+    if len(os.listdir(src)) == 0:
+        raise Exception(f"Source directory '{src}' is empty.")
+
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.makedirs(dest)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dest, item)
+        if os.path.isdir(s):
+            copy_to_directory(s, d)
+        else:
+            shutil.copy2(s, d)
+
+
+copy_to_directory('static', 'public')
