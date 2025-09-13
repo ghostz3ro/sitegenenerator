c87d9eb8fe8dcc86c454b0e7cefbc08cfa231c44
ghostz3ro <thasotiropoulos@gmail.com>
2025-09-13 15:22:02 +0300

    publish site to github pages

--- src/generate_html_page.py
+++ src/generate_html_page.py
@@ -1,6 +1,26 @@
-
 import os
 from markdown_to_html import markdown_to_html_node
+import shutil
+
+def copy_to_directory(src, dest):
+    
+    if not os.path.exists(src):
+        raise Exception(f"Source directory '{src}' does not exist.")
+    if len(os.listdir(src)) == 0:
+        raise Exception(f"Source directory '{src}' is empty.")
+
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.makedirs(dest)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dest, item)
+        if os.path.isdir(s):
+            copy_to_directory(s, d)
+        else:
+            shutil.copy2(s, d)
+
+
 
 def extract_title(markdown):
     for line in markdown.split("\n"):
@@ -8,7 +28,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("No title found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         markdown = f.read()
@@ -16,7 +36,8 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
     html_markdown = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    html_content = template.replace("{{ Title }}", title).replace("{{ Content }}", html_markdown)
+    html_content_temp = template.replace("{{ Title }}", title).replace("{{ Content }}", html_markdown)
+    html_content = html_content_temp.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     # print(html_content)
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     print(f"Writing to {dest_path}")
@@ -25,7 +46,7 @@ def generate_page(from_path, template_path, dest_path):
         f.close()
     return
 
-def generate_pages_recursive(from_path, template_path, dest_path):
+def generate_pages_recursive(basepath,from_path, template_path, dest_path):
     print(f"Generating pages recursively from {from_path} to {dest_path} using template {template_path}")
     print(f'checking path {from_path}')
     if os.path.exists(from_path):
@@ -38,10 +59,10 @@ def generate_pages_recursive(from_path, template_path, dest_path):
                 print('is file true')
                 print(f"Processing file {entry}")
                 dest_file = entry[:-3] + ".html"
-                generate_page(os.path.join(from_path, entry), template_path, os.path.join(dest_path, dest_file))
+                generate_page(basepath, os.path.join(from_path, entry), template_path, os.path.join(dest_path, dest_file))
             else:
                 print('is dir true')
-                generate_pages_recursive(os.path.join(from_path, entry), template_path, os.path.join(dest_path, entry))
+                generate_pages_recursive(basepath, os.path.join(from_path, entry), template_path, os.path.join(dest_path, entry))
     else:
         raise Exception(f"Folder {from_path} does not exist")
     print(f"Finished generating pages from {from_path} to {dest_path}")
--- src/main.py
+++ src/main.py
@@ -1,25 +1,14 @@
 import os
-import shutil
-from generate_html_page import generate_pages_recursive
-
-def copy_to_directory(src, dest):
-    
-    if not os.path.exists(src):
-        raise Exception(f"Source directory '{src}' does not exist.")
-    if len(os.listdir(src)) == 0:
-        raise Exception(f"Source directory '{src}' is empty.")
-
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    os.makedirs(dest)
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dest, item)
-        if os.path.isdir(s):
-            copy_to_directory(s, d)
-        else:
-            shutil.copy2(s, d)
+from generate_html_page import generate_pages_recursive, copy_to_directory
+import sys
 
 
-copy_to_directory('static', 'public')
-generate_pages_recursive('content', 'template.html', 'public')
+basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+print(f'Base path set to: {basepath}')
+
+print('static')
+copy_to_directory('static', 'docs')
+generate_pages_recursive(basepath, 'content', 'template.html', 'docs')
+
+# copy_to_directory('static', 'public')
+# generate_pages_recursive('content', 'template.html', 'public')
